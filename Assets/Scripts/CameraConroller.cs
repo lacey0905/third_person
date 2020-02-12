70160eb328fa874ba492cff510440e966d9516c4
username <user@email.mail>
2020-02-12 17:01:16 +0900

    Third Person Shooter

---
+++ Assets/Scripts/CameraConroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConroller : MonoBehaviour
+{
+    public GameObject target;
+
+    public float fSpeed;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * fSpeed);
+
+        Vector3 targetForward = target.transform.forward;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    Animator anim;
+
+    public GameObject model;
+
+    private Vector3 movement;
+
+    public float fSpeed;
+    public float fRotSpeed;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        anim = model.GetComponent<Animator>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        movement.Set(h, 0, v);
+        movement = movement.normalized * fSpeed * Time.deltaTime;
+
+        rigidbody.MovePosition(transform.position + movement);
+
+
+        if(h == 0 && v == 0)
+        {
+            anim.SetBool("Run", false);
+        }
+        else
+        {
+
+            anim.SetBool("Run", true);
+
+            Quaternion newRot = Quaternion.LookRotation(movement);
+            rigidbody.rotation = Quaternion.Slerp(rigidbody.rotation, newRot, fRotSpeed * Time.deltaTime);
+        }
+
+    }
+
+
+}
